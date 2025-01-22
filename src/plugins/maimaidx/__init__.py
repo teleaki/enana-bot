78908c1d27e5f7fb325d12a427727daee0be1cdd
teleaki <13513579476@163.com>
2025-01-23 06:17:02 +0800

    完善功能

--- src/plugins/maimaidx/__init__.py
+++ src/plugins/maimaidx/__init__.py
@@ -1,4 +1,4 @@
-from nonebot import get_plugin_config
+from nonebot import get_plugin_config, get_driver
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
@@ -17,6 +17,15 @@ from nonebot.adapters.onebot.v11 import Bot, Message, Event
 from nonebot.params import CommandArg
 
 from .lib.maimaidx_best50 import *
+
+
+driver = get_driver()
+
+@driver.on_startup
+async def get_data():
+    await mai.get_music_list()
+    # await mai.get_music_alias()
+
 
 b50 = on_command(
     "b50",
