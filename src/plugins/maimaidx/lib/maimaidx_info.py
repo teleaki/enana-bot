0c60d181a2479883acb7e1dcdefed48444ff577c
teleaki <13513579476@163.com>
2025-01-30 19:39:12 +0800

    查歌功能加入图片

--- src/plugins/maimaidx/lib/maimaidx_info.py
+++ src/plugins/maimaidx/lib/maimaidx_info.py
@@ -103,6 +103,7 @@ def song_info_draw(music: Music) -> Image.Image:
     if len(music.level) > 4:
         yh_font.draw(280, 1480, 30, f'{music.level[4]}({music.ds[4]})', (255, 255, 255, 255), 'mm')
         yh_font.draw(280, 1410, 30, f'{music.charts[4].charter}', (255, 255, 255, 255), 'mm')
+        draw_stats_info(yh_font, music, 135 * 4, 4)
 
     return img
 
